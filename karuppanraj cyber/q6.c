9be8665b67257af9be7a8be973292fd33e59ab19
Karuppanraj <karuppanraj721@gmail.com>
2025-09-10 10:21:00 +0530

    Add files via upload

---
+++ karuppanraj cyber/q6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b,c;
+	scanf("%d%d",&a,&b);
+	printf ("before swapping :\n");
+	a=b;
+	scanf("%d%d",&b,&c);
+	b=c;
+	printf("after swapping :\n");
+	printf("%d%d",a,b,c);
+	return 0;
+	
+	
+}
